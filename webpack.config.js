775b0b87e2fd8ef81af52884349b08335bfe771b
Jacob Arriola <jacob@zeek.com>
2020-02-06 07:41:18 -0800

    Update webpack with terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const purgecss = require('@fullhuman/postcss-purgecss')
 
 const env = process.env.NODE_ENV
