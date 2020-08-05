ac8e33034d2ce02295a69ecb3c0917ae3998d82b
Jacob Arriola <jacob@zeek.com>
2020-08-04 20:20:18 -0700

    Performance: use async + defer attributes for JS files

--- src/enqueue.php
+++ src/enqueue.php
@@ -9,7 +9,7 @@ add_action( 'wp_enqueue_scripts', function () {
 		[],
 		THEME_VERSION
 	);
-
+	
 	wp_enqueue_script(
 		'app-js',
 		THEME_URL . '/dist/bundle.js',
@@ -30,17 +30,17 @@ add_action( 'wp_enqueue_scripts', function () {
 		THEME_VERSION,
 		false
 	);
-});
+} );
 
 add_action( 'wp_head', function () {
 	$main_css = get_file_contents( '/dist/main.css' );
-
+	
 	if ( empty( $main_css ) ) {
 		return false;
 	}
-
+	
 	echo sprintf( '<style>%s</style>', $main_css );
-});
+} );
 
 /**
  * @param $path
@@ -48,15 +48,40 @@ add_action( 'wp_head', function () {
  * @return false|string|void
  */
 function get_file_contents( $path ) {
-
+	
 	$path = sprintf( '%s/%s',
 		get_template_directory(),
 		$path
 	);
-
+	
 	if ( empty( file_exists( $path ) ) ) {
 		return;
 	}
-
+	
 	return file_get_contents( $path );
 }
+
+/**
+ * Add defer attribute to our bundle
+ */
+add_filter( 'script_loader_tag', function ( $tag, $handle ) {
+	if ( 'app-js' !== $handle ) {
+		return $tag;
+	}
+	
+	return str_replace( '<script', '<script defer', $tag );
+}, 10, 2 );
+
+/**
+ * Add async attribute to our lazysizes
+ *
+ * Use async instead of defer b/c we want to to execute as soon as the script is found,
+ * since images depend on it.
+ */
+add_filter( 'script_loader_tag', function ( $tag, $handle ) {
+	if ( 'lazysizes' !== $handle ) {
+		return $tag;
+	}
+	
+	return str_replace( '<script', '<script async', $tag );
+}, 10, 2 );
