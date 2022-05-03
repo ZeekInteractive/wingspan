49ed42aff7a305022bd5d71010e25de536172e20
Steve Zehngut <steve@zeek.com>
2022-05-02 18:32:11 -0700

    Update all node modules, tailwind config and webpack config (ready for node v16)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,62 @@
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const {fontFamily} = require( 'tailwindcss/defaultTheme' )
 
 const safelist = []
 
 const whitelistPatterns = [
-  /^home(-.*)?$/,
-  /^blog(-.*)?$/,
-  /^archive(-.*)?$/,
-  /^date(-.*)?$/,
-  /^error404(-.*)?$/,
-  /^admin-bar(-.*)?$/,
-  /^search(-.*)?$/,
-  /^nav(-.*)?$/,
-  /^wp(-.*)?$/,
-  /^screen(-.*)?$/,
-  /^navigation(-.*)?$/,
-  /^(.*)-template(-.*)?$/,
-  /^(.*)?-?single(-.*)?$/,
-  /^postid-(.*)?$/,
-  /^post-(.*)?$/,
-  /^attachmentid-(.*)?$/,
-  /^attachment(-.*)?$/,
-  /^page(-.*)?$/,
-  /^(post-type-)?archive(-.*)?$/,
-  /^author(-.*)?$/,
-  /^category(-.*)?$/,
-  /^tag(-.*)?$/,
-  /^menu(-.*)?$/,
-  /^tags(-.*)?$/,
-  /^tax-(.*)?$/,
-  /^term-(.*)?$/,
-  /^date-(.*)?$/,
-  /^(.*)?-?paged(-.*)?$/,
-  /^depth(-.*)?$/,
-  /^children(-.*)?$/,
-  /^h[1-6]?$/,
+	/^home(-.*)?$/,
+	/^blog(-.*)?$/,
+	/^archive(-.*)?$/,
+	/^date(-.*)?$/,
+	/^error404(-.*)?$/,
+	/^admin-bar(-.*)?$/,
+	/^search(-.*)?$/,
+	/^nav(-.*)?$/,
+	/^wp(-.*)?$/,
+	/^screen(-.*)?$/,
+	/^navigation(-.*)?$/,
+	/^(.*)-template(-.*)?$/,
+	/^(.*)?-?single(-.*)?$/,
+	/^postid-(.*)?$/,
+	/^post-(.*)?$/,
+	/^attachmentid-(.*)?$/,
+	/^attachment(-.*)?$/,
+	/^page(-.*)?$/,
+	/^(post-type-)?archive(-.*)?$/,
+	/^author(-.*)?$/,
+	/^category(-.*)?$/,
+	/^tag(-.*)?$/,
+	/^menu(-.*)?$/,
+	/^tags(-.*)?$/,
+	/^tax-(.*)?$/,
+	/^term-(.*)?$/,
+	/^date-(.*)?$/,
+	/^(.*)?-?paged(-.*)?$/,
+	/^depth(-.*)?$/,
+	/^children(-.*)?$/,
+	/^h[1-6]?$/,
 ]
 
 module.exports = {
-  purge: {
-    content: ['**/*.php'],
-    preserveHtmlElements: true,
-    mode: 'layers',
-    layers: ['utilities'],
-    options: {
-      safelist,
-      whitelistPatterns,
-    }
-  },
+	content: ['**/*.php'],
 
-  theme: {
+	theme: {
 
-    extend: {
-      fontFamily: {
-        sans: [`Source Sans Pro`, ...fontFamily.sans],
-      },
-    },
+		extend: {
+			fontFamily: {
+				sans: [`Source Sans Pro`, ...fontFamily.sans],
+			},
+		},
 
-    container: {
-      center: true,
-      padding: `1rem`,
-    },
+		container: {
+			center: true,
+			padding: `1rem`,
+		},
 
-  },
+	},
 
-  variants: {},
+	variants: {},
 
-  plugins: [],
+	plugins: [],
 }
 
 
