6085c15763de1ebcea62d9735c3df31769be82e3
Steve Zehngut <steve@zeek.com>
2022-05-02 23:48:34 -0700

    Removed afeList and hitelistPatterns

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,5 @@
 const {fontFamily} = require( 'tailwindcss/defaultTheme' )
 
-const safelist = []
-
-const whitelistPatterns = [
-	/^home(-.*)?$/,
-	/^blog(-.*)?$/,
-	/^archive(-.*)?$/,
-	/^date(-.*)?$/,
-	/^error404(-.*)?$/,
-	/^admin-bar(-.*)?$/,
-	/^search(-.*)?$/,
-	/^nav(-.*)?$/,
-	/^wp(-.*)?$/,
-	/^screen(-.*)?$/,
-	/^navigation(-.*)?$/,
-	/^(.*)-template(-.*)?$/,
-	/^(.*)?-?single(-.*)?$/,
-	/^postid-(.*)?$/,
-	/^post-(.*)?$/,
-	/^attachmentid-(.*)?$/,
-	/^attachment(-.*)?$/,
-	/^page(-.*)?$/,
-	/^(post-type-)?archive(-.*)?$/,
-	/^author(-.*)?$/,
-	/^category(-.*)?$/,
-	/^tag(-.*)?$/,
-	/^menu(-.*)?$/,
-	/^tags(-.*)?$/,
-	/^tax-(.*)?$/,
-	/^term-(.*)?$/,
-	/^date-(.*)?$/,
-	/^(.*)?-?paged(-.*)?$/,
-	/^depth(-.*)?$/,
-	/^children(-.*)?$/,
-	/^h[1-6]?$/,
-]
-
 module.exports = {
 	content: ['**/*.php'],
 
